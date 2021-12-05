9bd914e15bcf00ea80f12f44a56e9e7c0faf6ea9
Yitro <yitro.verstraeten@student.howest.be>
2021-12-05 22:28:02 +0100

    finetuned the map display

--- script/app.js
+++ script/app.js
@@ -9,42 +9,35 @@ let listView = false;
 let list, divMap;
 
 const makeMarker = function (coords, adres, campusnaam) {
+  // eerst kijken hoe de coordinaten worden doorgegeven in het andere programma en dat hier op toepassen.
   // console.log(coords);
   const arr_coords = coords.split(',');
-  layergroup.clearLayers();
+  //layergroup.clearLayers();
   let marker = L.marker(arr_coords).addTo(layergroup);
   marker.bindPopup(`<h3>${campusnaam}</h3><em>${adres}</em>`);
 };
 
-const addEventsToCampus = function () {
-  const campussen = document.querySelectorAll('.c-campus__row');
-  for (const campus of campussen) {
-    campus.addEventListener('click', function () {
-      const coords = this.querySelector('.js-coords').innerHTML;
-      const adres = this.querySelector('.js-adres').innerHTML;
-      const campusnaam = this.querySelector('.js-campusnaam').innerHTML;
-      maakMarker(coords, adres, campusnaam);
-    });
-  }
-};
-
 const showVenues = function (data) {
+  let nameVenue = '';
+  let categoryVenue = '';
   console.log(data);
+  console.log(data.venues[0]);
+  let coords = `${data.venues[0].lat},${data.venues[0].lon}`;
+  console.log(coords);
+  makeMarker(coords, 'ff', 'fff');
+  //makeMarker('50.815950,3.316900', 'Luipaardstraat 12A, Kortrijk', 'Campus Kortrijk The Square');
+
   for (const venue of data.venues) {
-    console.log(venue);
-    //makeMarker(`${data.lat}, ${data.lon}`, 'ff', 'fff');
+    //console.log(venue);
+    coords = `${venue.lat},${venue.lon}`;
+    nameVenue = venue.name;
+    categoryVenue = venue.category;
+
+    makeMarker(coords, categoryVenue, nameVenue);
   }
 };
 
 const getAPI = function (lat, lon) {
-  //const url = `https://coinmap.org/api/v1/venues/?lat1=${lat}&lon1=${lon}`;
-  //const url = `https://coinmap.org/api/v1/venues/?lat1=50&lon1=4`;
-  //const url = 'https://coinmap.org/api/v1/coins/';
-
-  //const url = 'https://api.publicapis.org/entries';
-  //const url = 'https://cors-anywhere.herokuapp.com/https://coinmap.org/api/v1/venues/?lat1=40.0000&lon1=-75.0000&lat2=42.0000&lon2=-74.0000';
-
-  //const searchQuery = `https://cors-anywhere.herokuapp.com/https://coinmap.org/api/v1/venues/?lat1=${lat}&lon1=${lon}`;
   const url = 'https://testcoinmap.azurewebsites.net/api/v1/coin';
   console.log(url);
   const request = fetch(url)
@@ -91,6 +84,7 @@ const init = function () {
 
   map = L.map('mapid').setView([51.04028, 3.398512], 10);
   L.tileLayer(provider, { attribution: copyright }).addTo(map);
+  layergroup = L.layerGroup().addTo(map);
 
   document.querySelector('.js-btn').addEventListener('click', function () {
     toggleListView();
